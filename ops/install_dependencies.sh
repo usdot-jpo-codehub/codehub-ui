f188c2a606573361b502345275474636429b0609
Johnny Mohseni <jmohseni@gmail.com>
2016-06-21 19:20:10 -0400

    removing nginx install.  Missed last commit

--- ops/install_dependencies.sh
+++ ops/install_dependencies.sh
@@ -1,5 +1,2 @@
 # Client UI files already copied to /opt/heimdall - see ../appspec.yml
-
-yum -y install nginx
-# Using 'command cp' since these commands are run as root and 'cp' is aliased 'cp -i'
 cp -f /opt/heimdall/ops/nginx.conf /etc/nginx/nginx.conf
