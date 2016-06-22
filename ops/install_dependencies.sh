f94a55ca4d709564e4da7d312d57426eb53c3591
Johnny Mohseni <jmohseni@gmail.com>
2016-06-21 22:21:38 -0400

    more testing with codedeploy and running privileged commands

--- ops/install_dependencies.sh
+++ ops/install_dependencies.sh
@@ -1,2 +1,3 @@
 # Client UI files already copied to /opt/heimdall - see ../appspec.yml
-sudo cp -f /opt/heimdall/ops/nginx.conf /etc/nginx/nginx.conf
+yum -y install nginx
+cp -f /opt/heimdall/ops/nginx.conf /etc/nginx/nginx.conf
