2d548979127cb2a78ebcdd358f77573a48f02a11
Brent Conn <brent.conn@gmail.com>
2016-07-14 17:22:50 -0400

    #39 Project-Heimdall/home#6 Project-Heimdall/heimdall-webapp#80 Removing logging

--- src/modules/project/dataRepository/projectData.js
+++ src/modules/project/dataRepository/projectData.js
@@ -32,7 +32,6 @@ export class ProjectData {
   getAll() {
     return this.http.get(baseUrl)
       .then(response =>{
-        console.log(response.content);
         return response.content;
       })
   }
