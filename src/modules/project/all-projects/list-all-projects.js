6510bfd60923a80ce2627caa7bbd15b3192cd242
Brent Conn <brent.conn@gmail.com>
2016-07-25 15:54:56 -0400

    #82 Project-Heimdall/home#14 Project-Heimdall/home#6 Getting data for explore properly loaded and displayed

--- src/modules/project/all-projects/list-all-projects.js
+++ src/modules/project/all-projects/list-all-projects.js
@@ -1,9 +1,9 @@
 import {inject} from 'aurelia-framework';
-import {ProjectAllStaticData} from "../dataRepository/projectAllStaticData";
+import {ProjectsExplore} from "../dataRepository/projectsExplore";
 import {Router} from "aurelia-router";
 import {bindable} from 'aurelia-framework';
 
-@inject(ProjectAllStaticData, Router)
+@inject(ProjectsExplore, Router)
 export class ListAllProjects {
   heading = 'Projects List';
   projectsList = [];
@@ -15,12 +15,13 @@ export class ListAllProjects {
 getViewStrategy() {
       return '../common/list.html';
   }
-  constructor(data, router) {
-    this.service = data;
-    this.currentPage = 0;
+  constructor(projectsExplore, router) {
+    this.projectsExplore = projectsExplore;
     this.router = router;
     this.projects = [];
-    this.orgs = ["boozallen", "booz-allen-hamilton", "netflix"];
+
+    this.selectedOrganizations = [];
+    this.selectedLanguages = [];
   };
 
   gotoProject(project){
@@ -31,36 +32,34 @@ getViewStrategy() {
     this.router.navigateToRoute('create');
   };
 
-  getData(org) {
-    this.currentPage++;
-    return this.service.getAll(org)
+  getData() {
+    return this.projectsExplore.getAll()
       .then(projects => {
           this.projects = JSON.parse(JSON.stringify(projects));
-          return this.projects;
-     }).then(projects =>{
-        projects.forEach(proj =>{
-          this.getReadMeUrl(proj.full_name)
-          .then(readme_url =>{
-            proj.readme_url = readme_url;
-            return proj;
-          });
-        return projects;
-        })
-     });
 
+        this.selectedOrganizations = this.getUniqueValues(this.projects, 'organization');
+        this.selectedLanguages = this.getUniqueValues(this.projects, 'language');
+
+          return this.projects;
+     });
   }
 
-  getReadMeUrl(repo_full_name){
-    return this.service.getReadMeUrl(repo_full_name)
-      .then(projects_readme => {
-          return projects_readme;
-     })
+  // Creates an array of unique values for one property in an array
+  getUniqueValues(array, property){
+
+    let propertyArray = [];
+    for (let object of array) {
+      if(object[property]) {
+        propertyArray.push(object[property]);
+      }else{
+        propertyArray.push("None");
+      }
+    }
+    return Array.from(new Set(propertyArray));
+
   }
 
   activate() {
-
-    for(var i=0;i<this.orgs.length; i++){
-      this.getData(this.orgs[i]);
-    }
+      this.getData();
   }
 }
