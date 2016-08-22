6bb327d082f653e86a779a229a831627ddfb55cd
Brent Conn <brent.conn@gmail.com>
2016-08-22 15:00:46 -0400

    #164 Logic added for nav bar search

---
+++ src/nav-bar.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+export class NavBar {
+
+  attached() {
+    $('#searchBtn').on('click', event => {
+      $(this).addClass('hidden');
+      $('#titleBarNav').addClass('hidden');
+      $(this).closest($('.container-fluid')).find($('#titleBarNav')).addClass('hidden');
+      $('#searchForm').removeClass('hidden');
+      $('#searchForm input').focus();
+    });
+
+    $('#searchForm input').focusout(event => {
+      $('#searchBtn').removeClass('hidden');
+      $('#titleBarNav').removeClass('hidden');
+      $('#searchForm').addClass('hidden');
+    });
+  }
+
+}
