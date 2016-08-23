a94fc94fd818faf11a0c9c3bb30263141516b540
Brent Conn <brent.conn@gmail.com>
2016-08-23 11:58:51 -0400

    #164 Functionality for nav bar search added

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -1,6 +1,22 @@
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
 import $ from 'jquery';
+import { DataContext } from 'services/datacontext';
 
+@inject(DataContext, Router)
 export class NavBar {
+
+  constructor(dataContext, router) {
+    this.dataContext = dataContext;
+    this.router = router;
+
+    this.searchText = '';
+  }
+
+  executeSearch(searchText) {
+    this.router.navigateToRoute('results', { searchText });
+    $('#searchBox .typeahead').typeahead('close');
+  }
 
   attached() {
     $('#searchBtn').on('click', event => {
