091133b1bc0983e95bb3fb2f484c8768c6a5e02a
Brent Conn <brent.conn@gmail.com>
2016-11-14 13:58:12 -0500

    Project-Heimdall/home#140 Performing two searches in a row on the search page can mess up the results based on how the results are virtualized, reinvoking the lifecycle fixes this

--- src/search/results.js
+++ src/search/results.js
@@ -37,7 +37,7 @@ export class Results {
   }
 
   determineActivationStrategy() {
-    return activationStrategy.invokeLifecycle;
+    return activationStrategy.replace;
   }
 
   activate(params) {
