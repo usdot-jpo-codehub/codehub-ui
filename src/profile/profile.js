15dbee6676c03dba3eccf13b2924b579b70efc53
Brent <brent.conn@gmail.com>
2016-12-14 17:15:47 -0500

    Project-Heimdall/home#152 Variable name fix

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,9 +1,9 @@
 export class Profile {
 
-  constructor(dataContext, router, eventAggregator) {
-    this.username = PF_AUTH_GIVENNAME !== '' ? PF_AUTH_GIVENNAME : 'First'; // eslint-disable-line
-    this.username = PF_AUTH_SN !== '' ? PF_AUTH_SN : 'Last'; // eslint-disable-line
-    this.username = PF_AUTH_SUBJECT !== '' ? PF_AUTH_SUBJECT : 'Email'; // eslint-disable-line
+  constructor() {
+    this.PF_AUTH_GIVENNAME = PF_AUTH_GIVENNAME !== '' ? PF_AUTH_GIVENNAME : 'First'; // eslint-disable-line
+    this.PF_AUTH_SN = PF_AUTH_SN !== '' ? PF_AUTH_SN : 'Last'; // eslint-disable-line
+    this.PF_AUTH_SUBJECT = PF_AUTH_SUBJECT !== '' ? PF_AUTH_SUBJECT : 'Email'; // eslint-disable-line
   }
 
 }
