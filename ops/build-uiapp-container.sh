177cef1b58a5136accf0449d04758db5c3620b61
Brent <brent.conn@gmail.com>
2016-12-22 17:20:33 -0500

    Project-Heimdall/home#115 Minor documentation adjustments

--- ops/build-uiapp-container.sh
+++ ops/build-uiapp-container.sh
@@ -8,7 +8,7 @@
 # Pre-Conditions:
 #   1: Latest Node 4.x is installed
 #   2: Run this script from the root of the project because it expects the Dockerfile at the root of the project and
-#      the files it copies are relative to the root.
+#      the files it copies are relative to the root. E.g. sudo ./ops/build-uiapp-container.sh 0.1.0
 #
 # Arguments:
 #   1: $1 STAGE_UIAPP_VERSION - The version for this application (e.g. 0.1.0).
