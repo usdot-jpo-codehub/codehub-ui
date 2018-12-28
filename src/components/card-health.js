4d335999ad2964dfef52fa4d70f29165264b3736
Gio <rodriguez_carlos@bah.com>
2018-12-28 18:01:33 -0500

    #971 Card-Health : Handling invalid data and possible Number conversion error.

--- src/components/card-health.js
+++ src/components/card-health.js
@@ -1,10 +1,22 @@
 export class Card {
   constructor() {
     this.repo = [];
+    this.bugs = 0;
+    this.vulnerabilities = 0;
   }
 
   activate(modelData) {
     this.repo = modelData;
+    if (this.repo !== undefined) {
+      if (this.repo.metrics) {
+        if (this.repo.metrics.bugs) {
+          this.bugs = Number(this.repo.metrics.bugs.val);
+        }
+        if (this.repo.metrics.vulnerabilities) {
+          this.vulnerabilities = Number(this.repo.metrics.vulnerabilities.val);
+        }
+      }
+    }
   }
 
 }
