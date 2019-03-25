129393fa0c72361f81f5cc838cb64ce16e419f3a
583058 <solomonnathaniel@C02S50MXG8WM.local>
2019-03-25 11:38:09 -0400

    configs

--- install_run_script.sh
+++ install_run_script.sh
@@ -4,7 +4,7 @@
 #cp /tmp/aurelia-skeleton/skeleton-es2016 /app
 #JSPM_GITHUB_AUTH_TOKEN='2fcaec9eeb9a1580f3f79edfc8abfc3943e1769f'
 cd /app
-#npm install -y
+npm install -y
 #npm install --save-dev
 #npm install --save-dev gulp
 #npm install jspm --save-dev
