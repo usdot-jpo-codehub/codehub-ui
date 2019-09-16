00768709086463efdf8f4cbd4406bc4f5ad5354a
Mary VanDyke <vandyke_mary@bah.com>
2019-09-16 15:03:36 -0400

    Removed comments.

--- src/popular/popular.js
+++ src/popular/popular.js
@@ -6,10 +6,7 @@ import { StageConfig } from '../../stageConf';
 import { ReadmeModal } from '../components/modals/readme-modal';
 import { LeavingModal } from '../components/modals/leaving-modal';
 import { VScanModal } from '../components/modals/vscan-modal';
-//import json data fake file nickname here. 
 import { FakeData } from '../../fakeData';
-//google how to import json file
-
 
 @inject(DataContext, Router, StageConfig, DialogService, FakeData)
 export class Popular {
@@ -50,7 +47,7 @@ export class Popular {
   getData() {
     this.searchingPopular = true;
     this.dataContext.findPopular().then(results => {
-      if(!results) {
+      if (!results) {
         this.searchingPopular = false;
         return this.projects;
       }
@@ -61,105 +58,36 @@ export class Popular {
         return this.projects;
       }, 10);
     });
-    //inject fake data here at position 4
-    //need to syncronise these two: API & this array repo, then insert
-    // for (let i = 0; i < this.fp.length; i++) {
-    //   this.dataContext.findById(this.fp[i]).then(repo => {
-    //     this.featured.push(repo);
-
-    //   });
-    // }
-    //Gio:slack_call: 12:45 PM
-   let c = 0;
-   let feat = [];
-   this.searchingFeatured = true;
-   this.dataContext.findByIds(this.fp).then(resp => {
-     if (resp) {
-      let fakeData = this.fakeData;
-      // let fakeData = {
-      //   isFake :true, 
-      //   project_name: "Your repository name here!",
-      //   language: "Primary Language Here",
-      //   project_description: "Contribute to the advancement of U.S. Transportation!",
-      //   organization: "your username",
-      //   updatedAt: "Future Updates Pending",
-      //   stars: "?",
-      //   watchers: "?",
-      //   releases: "?",
-      //   forkedRepos: "?",
-      //   contributors: "?",
-      //   commits: "?"
-      // }; 
-   //let fakeData = fakeDatafilename.json;
-      let b = 4;
-      console.log(resp);
-      if(resp.length < b)
-      {
-        b=resp.length - 1;
+    let c = 0;
+    let feat = [];
+    this.searchingFeatured = true;
+    this.dataContext.findByIds(this.fp).then(resp => {
+      if (resp) {
+        let fakeData = this.fakeData;
+        let b = 4;
+        console.log(resp);
+        if (resp.length < b) {
+          b = resp.length - 1;
+        }
+        resp.splice(b, 0, fakeData);
+        console.log(resp);
+        this.featured = resp;
+        this.searchingFeatured = false;
       }
-      resp.splice(b, 0, fakeData);
-      console.log(resp);
-      this.featured = resp;
-      this.searchingFeatured = false;
-    }
-  });
-
-   
-   
-  //  let c = 0;
-  //  let feat = [];
-  //  this.searchingFeatured = true;
-  //  for (let i = 0; i < this.fp.length; i++) {
-  //    this.dataContext.findById(this.fp[i]).then(repo => {
-  //      c++;
-  //      if(repo) {
-  //        feat.push(repo);
-  //      }
-  //      if (c >= this.fp.length) {
-
-         //insert into 'feat' variable into position 4 the fake data (needs a 'name')
-         //insert json object. create variable for json oject, add variable "isFake = true;"
-      //    let fakeData = {
-      //         isFake :true, 
-      //         project_name: "Your repository name here!",
-      //         language: "Primary Language Here",
-      //         project_description: "Contribute to the advancement of U.S. Transportation!",
-      //         organization: "your username",
-      //         updatedAt: "Future Updates Pending",
-      //         stars: "?",
-      //         watchers: "?",
-      //         releases: "?",
-      //         forkedRepos: "?",
-      //         contributors: "?",
-      //         commits: "?"
-      // }; //could put project_name
-      //    //let fakeData = fakeDatafilename.json;
-      //    let b = 4;
-      //    if(feat.length < b)
-      //    {
-      //      b=feat.length - 1;
-      //    }
-      //     feat.splice(b, 0, fakeData);
-         
-    //      this.featured = [...feat];
-    //      this.searchingFeatured = false;
-    //      console.log(this.featured);
-    //    }
-    //  });
-   //}
+    });
 
     this.dataContext.findHealthiest().then((results) => {
       // Injecting project_description and organizationUrl.
       if (results && results.length > 0) {
         this.searchingHealthiest = true;
         // removing invalid elements
-        let filterRes = results.filter((x => {return x && x.id}));
-        let mapIds = filterRes.map( (x) => x.id);
-        if (mapIds && mapIds.length>0) {
+        let filterRes = results.filter((x => { return x && x.id }));
+        let mapIds = filterRes.map((x) => x.id);
+        if (mapIds && mapIds.length > 0) {
           this.dataContext.findByIds(mapIds).then((resp) => {
-            if(resp) {
+            if (resp) {
               filterRes.forEach((element) => {
-                let proj = resp.find(x => x.id === element.id );
+                let proj = resp.find(x => x.id === element.id);
                 if (proj) {
                   element.project_description = proj.project_description;
                   element.organizationUrl = proj.organizationUrl;
@@ -184,7 +112,7 @@ export class Popular {
     this.openReadmeLinkId = target.getAttribute('id');
     this.dialogService.open({ viewModel: ReadmeModal, model: repo, lock: false }).whenClosed(response => {
       if (response.wasCancelled) {
-        const element = document.querySelector('#'+this.openReadmeLinkId);
+        const element = document.querySelector('#' + this.openReadmeLinkId);
         element.focus();
       }
     });
@@ -193,9 +121,9 @@ export class Popular {
   openLeavingSiteConfirmation(name, url, target) {
     this.exitDialogLinkId = target.getAttribute('id');
     const mdl = { name, url };
-    this.dialogService.open({ viewModel: LeavingModal, model: mdl, lock: false }).whenClosed( response => {
-      const element = document.querySelector('#'+this.exitDialogLinkId);
-      if(element) {
+    this.dialogService.open({ viewModel: LeavingModal, model: mdl, lock: false }).whenClosed(response => {
+      const element = document.querySelector('#' + this.exitDialogLinkId);
+      if (element) {
         element.focus();
       }
     });
@@ -203,9 +131,9 @@ export class Popular {
 
   displayVScanDialog(repo, target) {
     this.exitDialogLinkId = target.getAttribute('id');
-    this.dialogService.open({viewModel: VScanModal, model: repo, lock: false}).whenClosed( reponse => {
-      const element = document.querySelector('#'+this.exitDialogLinkId);
-      if(element) {
+    this.dialogService.open({ viewModel: VScanModal, model: repo, lock: false }).whenClosed(reponse => {
+      const element = document.querySelector('#' + this.exitDialogLinkId);
+      if (element) {
         element.focus();
       }
     });
