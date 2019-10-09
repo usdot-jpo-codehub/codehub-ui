8ea8c2201d4838cc1b99bed028661d87a6f1e25d
Gio <rodriguez_carlos@bah.com>
2019-10-09 16:57:58 -0400

    reduce regular expresion

--- src/resources/value-converters/num.js
+++ src/resources/value-converters/num.js
@@ -9,10 +9,14 @@ export class NumValueConverter {
       { value: 1E6, symbol: 'M' },
       { value: 1E3, symbol: 'K' },
     ];
-    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    const rx = /(\.[0-9]*[1-9])0+$/;
     for (let i = 0; i < si.length; i++) {
       if (num >= si[i].value) {
-        return (num / si[i].value).toFixed(digits).replace(rx, '$1') + si[i].symbol;
+        let n = (num / si[i].value);
+        let f = n.toFixed(digits);
+        let r = f.replace(rx, '$1');
+
+        return r + si[i].symbol;
       }
     }
     return num.toFixed(digits).replace(rx, '$1');
