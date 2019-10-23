e7af209c150547752fd82e5b853716c079527396
Gio <rodriguez_carlos@bah.com>
2019-10-23 11:24:56 -0400

    RDA-1235 : Changes to configure nginx proxy_pass for the new API

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 envsubst '${PROXY_PASS_URL}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
+envsubst '${PROXY_PASS_URL_CC}' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
