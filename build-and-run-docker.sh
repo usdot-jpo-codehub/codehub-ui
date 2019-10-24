a6d9448ecee2248a589f9348436dfaea35bcb164
Matthew Schwartz <schwartz_matthew2@bah.com>
2019-10-24 12:39:29 -0400

    Cleanup shell scripts

--- build-and-run-docker.sh
+++ build-and-run-docker.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 docker build -t codehub-ui:latest .
-# docker run --rm -p 8080:80 -e PROXY_PASS_URL=$PROXY_PASS_URL -e PROXY_PASS_APICC_URL=$PROXY_PASS_APICC_URL codehub-ui:latest
-
-
-docker run -p 8080:80 -e PROXY_PASS_URL=$PROXY_PASS_URL -e PROXY_PASS_APICC_URL=$PROXY_PASS_APICC_URL codehub-ui:latest
+docker run --rm -d -p 8080:80 -e PROXY_PASS_URL=$PROXY_PASS_URL -e PROXY_PASS_APICC_URL=$PROXY_PASS_APICC_URL codehub-ui:latest
