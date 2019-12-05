e944a94f9cc6416e48d9027fd546e90d0d26496e
mvs5465 <mvs5465@gmail.com>
2019-12-05 13:01:35 -0500

    Update environment variables to allow nginx resolver directive

--- build-and-run-docker.sh
+++ build-and-run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t codehub-ui:latest .
-docker run --rm -d -p 8080:80 -e PROXY_PASS_URL=$PROXY_PASS_URL -e PROXY_PASS_APICC_URL=$PROXY_PASS_APICC_URL codehub-ui:latest
+docker run --rm -d -p 8080:80 -e PROXY_PASS_WEBAPI=$PROXY_PASS_WEBAPI -e PROXY_PASS_APICC=$PROXY_PASS_APICC codehub-ui:latest
