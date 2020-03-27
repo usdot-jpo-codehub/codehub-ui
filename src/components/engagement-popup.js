0ff86f2728385e76913451bb261cebb22c2511d4
Gio <rodriguez_carlos@bah.com>
2020-03-27 16:53:37 -0400

    RDA-1526 : Manage user close or not-show options.

--- src/components/engagement-popup.js
+++ src/components/engagement-popup.js
@@ -1,5 +1,5 @@
 import { inject } from 'aurelia-framework';
-import { ES_MSG_ENGAGEMENTPOPUP_DATA } from '../constants/ch-constants';
+import { ES_MSG_ENGAGEMENTPOPUP_DATA, LS_CODEHUB_ENGAGEMENT_POPUP } from '../constants/ch-constants';
 import {EventAggregator} from 'aurelia-event-aggregator';
 
 @inject(Element, EventAggregator)
@@ -9,11 +9,15 @@ export class EngagementPopup {
     this.eventAggregator = eventAggregator;
     this.data = null;
     this.eaSubscription = null;
+    this.isVisible = true;
+    this.isClosed = false;
+    this.noShowChecked = false;
   }
 
   attached() {
     this.eaSubscription = this.eventAggregator.subscribe(ES_MSG_ENGAGEMENTPOPUP_DATA, engagementPopups => {
       this.data = engagementPopups && engagementPopups.length>0 ? engagementPopups[0] : null;
+      this.isVisible = this.getUserSelection(this.data);
     });
   }
 
@@ -21,5 +25,29 @@ export class EngagementPopup {
     if (this.eaSubscription) {
       this.eaSubscription.dispose();
     }
+  }
+
+  close() {
+    this.isClosed = true;
+    setTimeout(()=>{
+      this.isVisible = false;
+    },500);
+  }
+
+  noShowClicked(event) {
+    this.noShowChecked = !this.noShowChecked;
+    if (this.data) {
+      window.localStorage.setItem(LS_CODEHUB_ENGAGEMENT_POPUP, this.data.id);
+      this.close();
+    }
+    return true;
+  }
+
+  getUserSelection(data) {
+    let val = window.localStorage.getItem(LS_CODEHUB_ENGAGEMENT_POPUP);
+    if (!val) {
+      return true;
+    }
+    return val != data.id;
   }
 }
